8de008fc09633d936eaf0880bd54a749b9f58002
stevensucipto31 <stevensucipto31@example.com>
2021-01-16 01:27:09 +0700

    revert back to deploy method

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -41,7 +41,7 @@ def main(script) {
 
 		stage('Deploy') {
 			println "Deliver to the world"
-			deployment.deployToLocalMachine(pipeline)
+			deployment.deploy(pipeline)
 		}
 
 		stage('health check') {
