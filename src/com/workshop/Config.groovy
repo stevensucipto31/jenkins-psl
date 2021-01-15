cfbb8647ecba93abb4717fc1e9406091a02f16eb
stevensucipto31 <stevensucipto31@gmail.com>
2021-01-15 18:03:06 +0700

    add default docker image & golang docker base image

--- src/com/workshop/Config.groovy
+++ src/com/workshop/Config.groovy
@@ -3,4 +3,10 @@ package com.workshop
 
 class Config {
     // Place Default Variable Here
+    // Docker related default variable
+   def default_docker_jenkins_tool = 'docker'
+ 
+   // Golang related default variable
+   def default_golang_base_image = "tobapramudia/tkpd-demo:onbuild"
+
 }
