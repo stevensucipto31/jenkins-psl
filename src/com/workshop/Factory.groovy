00cab10f0fe6dc77d7a2781b9dcf30e26ea5956a
stevensucipto31 <stevensucipto31@gmail.com>
2021-01-15 16:50:31 +0700

    get env var using env prefix

--- src/com/workshop/Factory.groovy
+++ src/com/workshop/Factory.groovy
@@ -8,13 +8,12 @@ def createConfig() {
 }
 
 def createPipeline() {
-	println env.repository_name
-	println env.branch_name
-	def repository_name = ("${repository_name}" != "null") ? "${repository_name}" : ""
-	def branch_name = ("${branch_name}" != "null") ? "${branch_name}" : ""
-	def git_user = ("${git_user}" != "null") ? "${git_user}" : ""
-	def docker_user = ("${docker_user}" != "null") ? "${docker_user}" : ""
-	def app_port = ("${app_port}" != "null") ? "${app_port}" : ""
+	
+	def repository_name = ("${env.repository_name}" != "null") ? "${repository_name}" : ""
+	def branch_name = ("${env.branch_name}" != "null") ? "${env.branch_name}" : ""
+	def git_user = ("${env.git_user}" != "null") ? "${env.git_user}" : ""
+	def docker_user = ("${env.docker_user}" != "null") ? "${env.docker_user}" : ""
+	def app_port = ("${env.app_port}" != "null") ? "${env.app_port}" : ""
 	def pr_num = ("${params.pr_num}" != "null") ? "${params.pr_num}" : ""
 
 	return new Pipeline(repository_name, branch_name, git_user, docker_user, app_port, pr_num)
