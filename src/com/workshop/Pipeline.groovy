b6f8f519a183fee7dda6c66c15eb07430089c2f8
stevensucipto31 <stevensucipto31@gmail.com>
2021-01-15 16:55:14 +0700

    remove this from print pipeline detail

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -12,19 +12,19 @@ class Pipeline {
     String pullRequestNumber
 
     Pipeline(String repositoryName, String branchName, String gitUsername, String dockerUsername, String appPort, String pullRequestNumber){
-	this.repositoryName = repositoryName
-	this.branchName = branchName
-	this.gitUsername = gitUsername
-	this.dockerUsername = dockerUsername
-	this.appPort = appPort
-	this.pullRequestNumber = pullRequestNumber
+        this.repositoryName = repositoryName
+        this.branchName = branchName
+        this.gitUsername = gitUsername
+        this.dockerUsername = dockerUsername
+        this.appPort = appPort
+        this.pullRequestNumber = pullRequestNumber
     }
     
     def printDetails() {
-	println("Repository Name: ${this.repositoryName}")
-	println("Branch Name: ${this.branchName}")
-	println("Git Username: ${this.gitUsername}")
-	println("Docker Username: ${this.dockerUsername}")
-	println("Pull Request Number: ${this.pullRequestNumber}")
+        println("Repository Name: ${repositoryName}")
+        println("Branch Name: ${branchName}")
+        println("Git Username: ${gitUsername}")
+        println("Docker Username: ${dockerUsername}")
+        println("Pull Request Number: ${pullRequestNumber}")
     }
 }
