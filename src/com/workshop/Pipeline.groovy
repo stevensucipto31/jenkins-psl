3b77f9b06fb17e4989aff2cc17cbe2ddf117df20
stevensucipto31 <stevensucipto31@gmail.com>
2021-01-15 16:19:17 +0700

    wrap println function in script tag

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -21,10 +21,12 @@ class Pipeline {
     }
     
     def printDetails() {
-    	println "Repository Name:" + repositoryName
+    	script {
+	println "Repository Name:" + repositoryName
 	println "Branch Name:" + branchName
 	println "Git Username:" + gitUsername
 	println "Docker Username:" + dockerUsername
 	println "Pull Request Number:" + pullRequestNumber
+	}
     }
 }
