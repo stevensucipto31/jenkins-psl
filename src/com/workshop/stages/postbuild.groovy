8dbb93c28557f5e35f2b5f7f71d85221d7e6c85a
stevensucipto31 <stevensucipto31@example.com>
2021-01-22 21:02:13 +0700

    fix variable name

--- src/com/workshop/stages/postbuild.groovy
+++ src/com/workshop/stages/postbuild.groovy
@@ -8,7 +8,7 @@ def merge(Pipeline pipeline){
   println "\u001b[36mMerge method using : \u001b[0mMerge..."
   
   // CALL GITHUB API TO MERGE PR
-  def pr_merge_response = httpRequest authentication: 'github-personal', httpMode: 'PUT', url: "https://api.github.com/repos/${pipeline.git_username}/${pipeline.repository_name}/pulls/${pipeline.pr_num}/merge", validResponseCodes: '200,405,409', wrapAsMultipart: false
+  def pr_merge_response = httpRequest authentication: 'github-personal', httpMode: 'PUT', url: "https://api.github.com/repos/${pipeline.git_user}/${pipeline.repository_name}/pulls/${pipeline.pr_num}/merge", validResponseCodes: '200,405,409', wrapAsMultipart: false
   
   def parsed_pr_merge_response = readJSON text: "${pr_merge_response.content}"
   if (parsed_pr_merge_response.containsKey("merged")){
@@ -21,7 +21,7 @@ def merge(Pipeline pipeline){
           error "Failed to merge, ${parsed_pr_merge_response['message']}"
       }
    } else {
-       def pr_check_response = httpRequest authentication: 'github-personal', httpMode: 'GET', url: "https://api.github.com/repos/${pipeline.git_username}/${pipeline.repository_name}/pulls/${pipeline.pr_num}", validResponseCodes: '200,405,409', wrapAsMultipart: false
+       def pr_check_response = httpRequest authentication: 'github-personal', httpMode: 'GET', url: "https://api.github.com/repos/${pipeline.git_user}/${pipeline.repository_name}/pulls/${pipeline.pr_num}", validResponseCodes: '200,405,409', wrapAsMultipart: false
        def parsed_pr_check_response = readJSON text: "${pr_check_response.content}"
  
        if ("${parsed_pr_check_response['merged']}" == "true") {
