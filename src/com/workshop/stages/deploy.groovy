f4e25681f9a0c09127eb3d71d83739d5eb6d1c97
stevensucipto31 <stevensucipto31@example.com>
2021-01-22 21:14:28 +0700

    fix variable name

--- src/com/workshop/stages/deploy.groovy
+++ src/com/workshop/stages/deploy.groovy
@@ -17,22 +17,7 @@ def deploy(Pipeline pipeline) {
    }
  
    docker.withTool("${config.default_docker_jenkins_tool}") {
-       def image = docker.image("${pipeline.docker_username}/${pipeline.repository_name}:build-$BUILD_NUMBER")
+       def image = docker.image("${pipeline.docker_user}/${pipeline.repository_name}:build-$BUILD_NUMBER")
        image.run("--name ${pipeline.repository_name}-$BUILD_NUMBER -p ${pipeline.app_port}:${pipeline.app_port}")
    }
-}
-
-def deployToLocalMachine(Pipeline pipeline) {
-    config = new Config()
-
-    def response = sh script: "docker rm -f \$(docker ps -aq -f 'name=${pipeline.repository_name}') &> /dev/null", returnStatus: true
-    if ("${response}" == "0") {
-        println("Successfuly removing old container")
-    } else {
-        println("Old version removed")
-    }
-
-    sh """
-        docker run --name ${pipeline.repository_name}-$BUILD_NUMBER -p ${pipeline.app_port}:${pipeline.app_port} -d ${pipeline.docker_username}/${pipeline.repository_name}:build-$BUILD_NUMBER
-    """
 }
