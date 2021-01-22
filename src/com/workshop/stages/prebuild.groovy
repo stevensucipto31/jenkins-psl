4cc6a2cbc0ff28208ba38fac500c2f576db66734
stevensucipto31 <stevensucipto31@example.com>
2021-01-22 20:35:15 +0700

    fix variable name

--- src/com/workshop/stages/prebuild.groovy
+++ src/com/workshop/stages/prebuild.groovy
@@ -11,6 +11,6 @@ def details(Pipeline p) {
    println("\u001b[36mBranch Name : \u001b[0m${p.branch_name}")
    println("\u001b[36mApplication Port : \u001b[0m${p.app_port}")
    println("")
-   println("\u001b[36mTesting this PR : \u001b[0m#${p.pr_num} - https://github.com/${p.git_username}/${p.repository_name}/pull/${p.pr_num}")
+   println("\u001b[36mTesting this PR : \u001b[0m#${p.pr_num} - https://github.com/${p.git_user}/${p.repository_name}/pull/${p.pr_num}")
    println("\u001b[36mMerging to branch : \u001b[0m${p.branch_name}")
 }
